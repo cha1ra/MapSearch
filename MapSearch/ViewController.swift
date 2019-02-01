afd81500eb86e34db1c1bc64dfa331ae2298d589
cha1ra <cha1ra.garnet@gmail.com>
2019-02-01 10:32:52 +0900

    Initial Commit

---
+++ MapSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapSearch
+//
+//  Created by 坂尻愛明 on 2019/02/01.
+//  Copyright © 2019 cha1ra.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
